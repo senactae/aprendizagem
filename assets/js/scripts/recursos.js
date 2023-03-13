5e02f64a832167734f40e261f9f24a3ff2c8be80
Fernando Milagres <milagres.fernando@gmail.com>
2023-03-13 14:16:11 -0300

    atualizando boots, ainda com erros

--- assets/js/scripts/recursos.js
+++ assets/js/scripts/recursos.js
@@ -1,31 +1,39 @@
 $(document).ready(function () {
-	renderImgLegenda();
-	flipCard();
+  renderImgLegenda();
+  flipCard();
 });
 
-function renderImgLegenda(){
-    $(".img-legenda").each(function(index) {
-		$wrapImg = $('<div>').addClass('img-wrapper').attr('id', 'imgW_' + index);
+function renderImgLegenda() {
+  $(".img-legenda").each(function (index) {
+    $wrapImg = $("<div>")
+      .addClass("img-wrapper")
+      .attr("id", "imgW_" + index);
 
-		if ($(this).data('cor')) {
-			$cor = $(this).data('cor');
-			$wrapImg.addClass($cor);
-		}
-		$(this).wrap($wrapImg);
+    if ($(this).data("cor")) {
+      $cor = $(this).data("cor");
+      $wrapImg.addClass($cor);
+    }
+    $(this).wrap($wrapImg);
 
-		if ($(this).data('legenda')) { 
-			$legendaImg = $('<span>').html($(this).data('legenda'));
-			
-			if ($(this).data('width'))
-				$legendaImg.addClass('minText').css('width', $(this).data('width'));
-			
-			$(this).parent().append($legendaImg);
-		}
-    });
+    if ($(this).data("legenda")) {
+      $legendaImg = $("<span>").html($(this).data("legenda"));
+
+      if ($(this).data("width"))
+        $legendaImg.addClass("minText").css("width", $(this).data("width"));
+
+      $(this).parent().append($legendaImg);
+    }
+  });
 }
 
-function flipCard(){
-	$(".cardFlip").bind("click", function(evt) {
-		$(this).toggleClass('giraCarta');
-	});
+function flipCard() {
+  $(".cardFlip").bind("click", function (evt) {
+    $(this).toggleClass("giraCarta");
+  });
 }
+
+/////////////////
+// FLIPCARD
+$(".card-selector .fcard").on("click", function () {
+  $(this).toggleClass("active");
+});
